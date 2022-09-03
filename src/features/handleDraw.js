2751acf356a1ba114ba0b4b85be8af979d461866
Kyuu <me@lkyuusei.fr>
2022-09-03 15:56:30 +0200

    feat(draw): add timer for hourly draw

--- src/features/handleDraw.js
+++ src/features/handleDraw.js
@@ -2,19 +2,14 @@ import { post, patch } from "../lib/fetch.js";
 import getDocumentRequest from "../services/db/getDocumentRequest.js";
 import setDocumentRequest from "../services/db/setDocumentRequest.js";
 
-import { logDatabase } from "../services/db/database.js";
+import { HOUR_IN_MS } from "../lib/constants.js";
 
 export const handleDrawChoice = async (postUrl, patchUrl, member, pick) => {
     const { id } = member.user;
-    await logDatabase("before");
 
     const document = await getDocumentRequest(id);
-    console.log("l12", document)
     document.cards = [...document.cards, { id: pick }];
-    console.log("l14", document)
     await setDocumentRequest(id, document);
-
-    await logDatabase("after");
 
     const interaction_response = await post(postUrl, {
         "type": 4,
@@ -32,37 +27,47 @@ export const handleDrawChoice = async (postUrl, patchUrl, member, pick) => {
 }
 
 export const handleDraw = async (url, member) => {
-    const response = await post(url, {
-        "type": 4,
-        "data": {
-            "content": `Commande: Tirer une carte, par <@${member.user.id}>`,
-            "components": [
-                {
-                    "type": 1,
-                    "components": [
-                        {
-                            "type": 2,
-                            "style": 1,
-                            "label": "Ryushi",
-                            "custom_id": "ryushi",
-                        },
-                        {
-                            "type": 2,
-                            "style": 2,
-                            "label": "Spica",
-                            "custom_id": "spicalyce",
-                        },
-                        {
-                            "type": 2,
-                            "style": 3,
-                            "label": "Savyr",
-                            "custom_id": "savyr",
-                        },
-                    ]
-                }
-            ]
-        }
-    });
+    const { id } = member.user;
+    const document = await getDocumentRequest(id);
 
-    return response;
+    const canDraw = document.lastDrawTimestamp + HOUR_IN_MS < Date.now();
+    const data = canDraw ? {
+        "content": `Commande: Tirer une carte, par <@${member.user.id}>`,
+        "components": [
+            {
+                "type": 1,
+                "components": [
+                    {
+                        "type": 2,
+                        "style": 1,
+                        "label": "Ryushi",
+                        "custom_id": "ryushi",
+                    },
+                    {
+                        "type": 2,
+                        "style": 2,
+                        "label": "Spica",
+                        "custom_id": "spicalyce",
+                    },
+                    {
+                        "type": 2,
+                        "style": 3,
+                        "label": "Savyr",
+                        "custom_id": "savyr",
+                    },
+                ]
+            }
+        ]
+    } : {
+        "content": `Vous pourrez tirer une carte dans ${Math.floor((document.lastDrawTimestamp + HOUR_IN_MS - Date.now()) / 60000)} minutes.`,
+    }
+
+    if (canDraw) {
+        await setDocumentRequest(id, { ...document, lastDrawTimestamp: Date.now() });
+    }
+
+    return await post(url, {
+        "type": 4,
+        data,
+    });
 }
