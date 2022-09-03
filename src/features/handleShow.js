0b86a9ea747f55516c4a73fa12859151b03dabe1
Kyuu <me@lkyuusei.fr>
2022-09-03 15:56:52 +0200

    feat(show): add points to show all, and handle non existant cards

--- src/features/handleShow.js
+++ src/features/handleShow.js
@@ -1,32 +1,33 @@
 import { post } from "../lib/fetch.js";
 import getDocumentRequest from "../services/db/getDocumentRequest.js";
 
-const showCard = (cardId) => {
-    return cardId;
+const showCard = async (id, cardId) => {
+    const document = await getDocumentRequest(id);
+    const card = document.cards.find(card => card.id === cardId);
+    return card ? { card: card.id } : { card: "Vous ne possédez pas cette carte." };
 }
 
 const showCards = async (id) => {
     const document = await getDocumentRequest(id);
-    return document.cards.map(card => card.id);
+    return { points: document.points, cards: document.cards.map(card => card.id) };
 }
 
 const handleShow = async (url, member, { options }) => {
     const { name, options: params } = options[0];
+    const { id } = member.user;
 
-    const output = name === 'all' ? await showCards(member.user.id) : showCard(params[0].value);
+    const { points, cards, card } = name === 'all' ? await showCards(id) : await showCard(id, params[0].value);
 
     const content = name === 'all' ?
-        `Commande: Toutes les cartes de <@${member.user.id}>: ${output.join(', ')}` :
-        `Commande: Afficher une carte (code: ${params[0].value}), par <@${member.user.id}>`;
+        `Commande: Toutes les cartes de <@${member.user.id}>: ${cards.join(', ')}, pour un total de ${points} points.` :
+        `Commande: Afficher une carte (code: ${params[0].value}), par <@${member.user.id}> : ${card}`;
 
-    if (name === 'all') {
-        return await post(url, {
-            "type": 4,
-            "data": {
-                content
-            }
-        });
-    }
+    return await post(url, {
+        "type": 4,
+        "data": {
+            content
+        }
+    });
 }
 
 export default handleShow;
